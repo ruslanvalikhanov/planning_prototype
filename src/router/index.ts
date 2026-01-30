1f9528c644157b8fb896e3be6b213936997e0f76
ruslanvalikhanov <ruslan.valikhanov@thermondo.de>
2026-01-30 18:37:38 +0100

    hash routing update

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import ECPrototype from '../views/ECPrototype.vue'
 import ProjectsList from '../components/ProjectsList.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/TC',
